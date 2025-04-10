2f5cbb81b5d06b554fc02481819aa38372749144
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-04-10 12:52:42 -0500

    Added the database details and done loading jobs' information dynamically

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask,render_template,jsonify
+import database
+from sqlalchemy import text 
 app = Flask(__name__)
 
-JOBS = [
+""" JOBS = [
     {'id': 1, 'title': 'Software Engineer', 'company': 'XYZ Corp.', 'location': 'New York','salary':'$450000'},
     {'id': 2, 'title': 'Data Scientist', 'company': 'ABC Inc.', 'location': 'San Francisco','salary':'$350000'},
     {'id': 3, 'title': 'Product Manager', 'company': 'PQR LTD.', 'location': 'London','salary':'$250000'},
     {'id': 4, 'title': 'Backend Engineer', 'company': 'Facebook', 'location': 'Boston','salary':'$650000'}
-]
+] 
+ """  
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html',jobs=JOBS,company_name='Pranav Consultancy')
+    jobs = database.load_jobs_from_db()
+    return render_template('home.html',jobs=jobs,company_name='Pranav Consultancy')
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = database.load_jobs_from_db()
+    return jsonify(jobs)
 
+@app.route('/api/jobs/<job_id>')
+def load_job(job_id):
+    job = database.load_job_from_db(job_id)
+    return jsonify(job)
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,7 +1,28 @@
 from sqlalchemy import create_engine,text
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("mysql+pymysql://root:pranavpratheek@127.0.0.1/pranavconsultancy")
+load_dotenv() #loads from the .env file
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    print(result.all())
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs")).mappings().all()
+        result_dicts = []
+    
+        for row in result:
+            result_dicts.append(dict(row))
+        
+        return result_dicts
+    
+def load_job_from_db(job_id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id= :val"),{"val" : job_id}).mappings().all()
+        rows = result
+    
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
