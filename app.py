f2a5c7d565009d70078afc4c64df5cf75cd05977
Pranav Pratheek Malleboyina <pranav.msadmissions@gmail.com>
2025-04-12 03:13:36 -0500

    made the changes for saving the submitted applications to the database

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,request
 import database
 from sqlalchemy import text 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #this is the root page
 def hello_world():
     jobs = database.load_jobs_from_db()
     return render_template('home.html',jobs=jobs)
 
-@app.route('/api/jobs')
+@app.route('/api/jobs')  #to display all the jobs from the database
 def list_jobs():
     jobs = database.load_jobs_from_db()
     return jsonify(jobs)
 
-@app.route('/api/jobs/<job_id>')
+@app.route('/api/jobs/<job_id>') #to retrieve a particular job from the database
 def load_job(job_id):
     job = database.load_job_from_db(job_id)
     if not job:
         return "Sorry,Not Found!!",404
-    
     return render_template('jobdetails.html',job=job)
+
+@app.route('/api/jobs/<job_id>/apply',methods=['post'])   
+def apply_to_job(job_id):
+     data = request.form
+     job = database.load_job_from_db(job_id)
+     status = database.save_application_to_db(job_id,data)
+     return render_template('application_submitted.html',job=job,application=data)
     
 if __name__ == '__main__':
     app.run(debug=True)
+    
--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine,text
 from dotenv import load_dotenv
 import os
+import logging
 
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 load_dotenv() #loads from the .env file
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -26,3 +29,21 @@ def load_job_from_db(job_id):
             return None
         else:
             return dict(rows[0])
+        
+def save_application_to_db(job_id,data): 
+    with engine.connect() as conn:
+        query = text("insert into applications(job_id,full_name,email,linkedin_url,education,work_experience,resume_url) values(:job_id,:full_name,:email,:linkedin_url,:education,:work_experience,:resume_url)") 
+        transaction = conn.begin()
+        try:
+            status = conn.execute(query,{
+                      "job_id":job_id,
+                      "full_name": data['full_name'],
+                      "email" : data['email'],
+                      "linkedin_url" :data['linkedin_url'],
+                      "education": data['education'],
+                      "work_experience": data['work_experience'],
+                      "resume_url" : data['resume_url']
+                      } )  
+            transaction.commit()   #used for all the insert statements
+        except:
+            transaction.rollback()         
